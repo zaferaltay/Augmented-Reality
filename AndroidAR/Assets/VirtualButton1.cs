c26e7b618a4f577ba567ec6f39b4b0bbd36ada35
Zafer Altay <32716446+zaferaltay@users.noreply.github.com>
2021-11-12 22:03:04 +0300

    HW1
    • At least one stationary virtual object.
    • At least one animated virtual object.
    • At least one moving object.
    • At least one textual virtual object.
    • At least one virtual object mapping a single picture.
    • At least one virtual object mapping and playing a video.
    • At least one step involving the (user) selection of a button to change scenarios.
    
    Tasks done.

---
+++ AndroidAR/Assets/VirtualButton1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VirtualButton1 : MonoBehaviour
+{
+    public void push1(){
+        SceneManager.LoadScene(2);
+    }
+   
+}
